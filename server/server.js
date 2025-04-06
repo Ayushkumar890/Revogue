a1d16d2a62f4ca8714094494b52f04ce49da2102
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2025-04-06 18:40:25 +0530

    change the folder name

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use('/product', productRoutes);
 app.use('/api/auth', userRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
