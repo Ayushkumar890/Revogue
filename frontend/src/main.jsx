12be0ff5d7181509871c240f0e2e13e65107312b
AstitvaRajSinha <astitvaraj870@gmail.com>
2025-04-11 09:31:56 +0530

    About page updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
